1a335d4c38fef954ff9a7da8328d741d0a0c797c
zimpel <zimpel@gmx.li>
2023-05-26 13:43:18 +0000

    index

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, Woreeld!</p>"
+def hello_zimpel():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
