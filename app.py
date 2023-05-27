b486a18d411d1b7242b474776c26b1f72a5d97f0
zimpel <zimpel@gmx.li>
2023-05-27 06:30:33 +0000

    more

--- app.py
+++ app.py
@@ -2,11 +2,37 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'berlin',
+    'gehalt': '120000'
+  },
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'berlin',
+    'gehalt': '120000'
+  },
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'berlin',
+    'gehalt': '120000'
+  },
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'berlin',
+    'gehalt': '120000'
+  },
+]
 
 
 @app.route("/")
 def hello_zimpel():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Zimpel')
 
 
 if __name__ == "__main__":
